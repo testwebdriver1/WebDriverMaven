6bf83eef5618a9863249172f6bed76f388463829
Suzan Chunduru <Suzan Chunduru>
2016-07-26 15:36:18 +0100

    first commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,51 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+	
+		driver= new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void doLogin(){
+		
+		driver.get("http://mail.yahoo.com");
+		driver.findElement(By.xpath("//*[@id='login-username']")).sendKeys("testwebdriver1");
+		driver.findElement(By.xpath("//*[@id='login-signin']")).click();
+		driver.findElement(By.xpath("//*[@id='login-passwd']")).sendKeys("justin@123");
+		//driver.findElement(By.xpath("//*[@id='login-signin']")).click();
+				
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		driver.quit();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
